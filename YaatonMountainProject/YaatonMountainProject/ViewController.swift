8c375fff08ef703247c52ad4849b7c8abf13fb96
웡빙 <wb277@naver.com>
2022-09-14 10:29:05 +0900

    feat: 코드로 뷰 구현을 위한 세팅
    - storyboard 제거

--- YaatonMountainProject/YaatonMountainProject/ViewController.swift
+++ YaatonMountainProject/YaatonMountainProject/ViewController.swift
@@ -7,15 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "테스트 텍스트"
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
+        setupInitialView()
+        setupMainStackView()
     }
-
-
+    
+    private func setupInitialView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupMainStackView() {
+        view.addSubview(mainStackView)
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+        ])
+    }
 }
-
