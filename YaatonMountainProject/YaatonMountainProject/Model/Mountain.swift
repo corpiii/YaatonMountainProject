4226a9837e5934e36b139f1e187ed1be3d1e1df5
jeongmin <lj845854@gmail.com>
2022-09-14 09:50:51 +0900

    feat: Mountain 모델 생성

---
+++ YaatonMountainProject/YaatonMountainProject/Model/Mountain.swift
@@ -0,0 +1,16 @@
+//
+//  Mountain.swift
+//  YaatonMountainProject
+//
+//  Created by 이정민 on 2022/09/14.
+//
+
+import UIKit
+
+struct Mountain {
+    let isKoreanMountain: Bool
+    let name: String
+    let height: Double
+    let image: UIImage
+    let description: String
+}
